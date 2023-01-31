7ecc3c14714e71f17ee7a21888733bec7e198dfd
Seopject <swork0308@gmail.com>
2023-01-31 14:03:08 +0900

    0131

--- bj/2775.py
+++ bj/2775.py
@@ -2,6 +2,19 @@
 # k와 n에 대해 k층 n호에 몇 명이 살고있는가 아파트 0층부터, 1호부터
 # 0층 i호는 i명이 산다
 
+T = int(input()) # tc
+for tc in range(T):
+    floor = int(input()) # 층수
+    num = int(input()) #호수
 
-# T = int(input()) # tc
+    floorlist_up = [0]*num
+    floorlist_down = list(range(1,num+1))
+
+    for n in range(floor):
+        for k in range(num):
+            floorlist_up[k] = sum(floorlist_down[0:k+1])
+        floorlist_down = floorlist_up
+        floorlist_up = [0]*num
+
+    print(floorlist_down[num-1])
 
--- bj/note.py
+++ bj/note.py
@@ -1,2 +1,30 @@
-height, width, customer = list(map(int, input().split()))
-print(height, width, customer)
+class Person:
+    def __init__(self):
+        self._age = 0
+        #protected
+    
+    @property
+    def age(self): #getter
+        print('getter 호출 !')
+        return self._age
+    
+    @age.setter
+    def age(self, age): #setter
+        print('setter 호출 !')
+        self._age = age
+
+    # 데코레이터 없을 때
+    # age = property(get_age, set_age)
+    
+    
+p1 = Person()
+# p1._age = 25 #protect에선 사용하면 안됨
+
+# 비교적 불편
+""" p1.set_age(25)
+print(p1.get_age())
+ """
+# 내부적으로 setter, getter 사용
+p1 = Person()
+p1.age = 25
+print(p1.age)

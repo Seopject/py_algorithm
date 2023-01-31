17d21e2b1e9aeda91464f8d86804d2d5f8d2f014
Seopject <swork0308@gmail.com>
2023-02-01 00:55:12 +0900

    0131

--- bj/11653.py
+++ bj/11653.py
@@ -1,7 +1,18 @@
-N = int(input())
-check = [0] * N
-print(check)
+# 1. 2부터 루트 N 까지의 소수 구하기
+# 2. 소인수분해 실행
 
+N = int(input())
+
+# 소수 구하기
+cnt = 0
+check = []
+for j in range(1, int(N**1/2)+1):
+    if N % j == 0:
+        check.append(j)
+if len(check) == 1:
+        cnt += 1
+
+print(check)
 
 
 """ N = int(input())
--- bj/note.py
+++ bj/note.py
@@ -1,5 +1,26 @@
-try:
-    num = input('숫자 : ')
-    print(int(num))
-except:
-    print('숫자가 아니다')
+# N까지 소수 구하기
+# 1)0~N까지의 배열 만들기 ([0] * (N+1))
+# i의 범위 2 ~ 루트N
+# i부터 루트N 전의 i배수 전부 1로
+# 다음 1이 아닌 요소 찾아서 반복
+
+import time
+start = time.time()
+
+N = int(input())
+test = [0]*(N+1)
+
+
+for i in range(2, int(N**1/2)+1):
+    if test[i] == 1:
+        continue
+
+    else:
+        for j in range(i+i,N+1,i):
+            test[j] = 1
+
+print(test)
+end = time.time()
+print(end - start)
+
+

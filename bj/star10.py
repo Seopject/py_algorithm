dc9cefc15faf66a9a6d1a13aa27793d2f9441376
Seopject <swork0308@gmail.com>
2023-02-07 17:53:46 +0900

    0207

--- bj/star10.py
+++ bj/star10.py
@@ -7,9 +7,13 @@ def stars(n):
         for i in range(N):
             print(space[i])
     else:
+        for r in range((n//3),2*(n//3)):
+            for c in range((n//3),2*(n//3)):
+                space[r][c] = ' '
         for k in range(3):
-            for r in range((n//3),2*(n//3)):
-                for c in range((n//3),2*(n//3)):
-                    space[r][c] = ' '
+            for x in range((n//3)*k+(n//(3*3)),(n//3)*k+2*(n//(3*3))):
+                for y in range((n//3)*k+(n//(3*3)),(n//3)*k+2*(n//(3*3))):
+                    space[x][y] = ' '
+
         return stars(n//3)
 stars(N)

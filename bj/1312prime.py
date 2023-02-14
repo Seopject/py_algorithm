511014daf87a13144052c3797903943f20ebeb96
Seopject <swork0308@gmail.com>
2023-02-14 17:55:56 +0900

    0214

--- bj/1312prime.py
+++ bj/1312prime.py
@@ -1,7 +1,12 @@
-try:
-    A,B,N = map(int,input().split())
-    ans = list(str(round(A/B,N+1)))
-    print(ans[ans.index('.')+N])
-except:
-    IndexError
-    print(0)
+A,B,N = map(int,input().split())
+num = list(str(round(A/B,N+2)))
+ans = [0]*(N+1)
+idx = num.index('.')
+for i in range(idx,len(ans)):
+    if i >= len(num) or len(ans):
+        break
+    ans[i-idx] = num[i]
+print(idx)
+print(num)
+print(ans)
+print(ans[-1])

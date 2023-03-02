29372b5dcb1c92aebdabceb8d247adbca72ae191
seopject <swork0308@gmail.com>
2023-03-02 17:50:34 +0900

    0302

--- samsung_A/castledefense.py
+++ samsung_A/castledefense.py
@@ -1,1 +1,7 @@
 N, M, D = map(int,input().split())
+arr = [list(map(int,input().split())) for _ in range(N)][::-1]
+for a in range(N-D+1):
+    temp = arr[a:a+D]
+    for i in range(D):
+        print(temp[i])
+    print()
